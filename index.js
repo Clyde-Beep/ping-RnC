4ea9556fef2ba6beee01d75ad792a90523c0a5fd
Not Kyza <71053963+NotKyza@users.noreply.github.com>
2020-09-09 19:51:34 -0700

    stuff

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Plugin } = require('powercord/entities')
+const { inject, uninject } = require('powercord/injector')
+const { React, getModule } = require('powercord/webpack')
+const insertText = e => getModule(['ComponentDispatch'], false).ComponentDispatch.dispatchToLastSubscribed("INSERT_TEXT", { content: e })
+
+module.exports = class PingKyza extends Plugin {
+    startPlugin() {
+        document.body.addEventListener("keyup", this.keyup);
+    }
+
+    keyup(event) {
+        if (event.key == 'F4') {
+            insertText('<@220584715265114113> ')
+        }
+    }
+
+    pluginWillUnload() {
+        document.body.removeEventListener("keyup", this.keyup);
+    }
+}
